7cac70a5e7cbc7ba6802573d1d4e483adbcfe34a
oleg <oleg_09-95@mail.ru>
2026-05-27 20:53:46 +0300

    Composing Components переиспользован компонент User в App

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,16 +2,40 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
+  selector: 'app-user',
+  template: `Username: {{ username }}`,
+})
+export class User {
+  username = 'Rembo';
+}
+
+
+@Component({
   selector: 'app-root',
-  template: `Hello Universe`,
+  template: `
+    <div class="left">Hello Universe. My city: {{ city }} Any: {{ 1 * 3 + 2 }}</div>
+    <div class="right">
+      <app-user />
+    </div>
+  `,
   styles: `
     :host {
+      height: 100vh;
       color: #a144eb;
       border: 2px dotted red;
       padding: 1rem;
+      display: flex;
+      gap: 10px;
+    }
+    .left, .right {
+      width: 50%;
+      border: 1px solid red;
+      height: max-content;
     }
   `,
+  imports: [User],
 })
 export class App {
   protected readonly title = signal('my-app');
+  city = 'San Francisco';
 }
