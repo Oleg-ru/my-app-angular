84c4975c08c862ec0437cf31014c5590c6de919e
oleg <oleg_09-95@mail.ru>
2026-05-30 20:13:00 +0300

    Формы Template-driven форма

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { disabled } from '@angular/forms/signals';
 
 @Component({
   selector: 'app-user',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user.html',
   styleUrl: './user.scss',
 })
-export class User {}
+export class User {
+  favoriteFramework = '';
+  showFramework = () => {
+    if (this.favoriteFramework)
+      alert(this.favoriteFramework);
+  }
+  protected readonly disabled = disabled;
+}
