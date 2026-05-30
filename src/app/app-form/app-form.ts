dd43a5c57155217846c0f2798426c5fd4325cff7
oleg <oleg_09-95@mail.ru>
2026-05-30 21:04:50 +0300

    Формы Валидация форм

--- src/app/app-form/app-form.ts
+++ src/app/app-form/app-form.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-app-form',
@@ -9,8 +9,8 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 })
 export class AppForm {
   profileForm = new FormGroup({
-    name: new FormControl(''),
-    email: new FormControl(''),
+    name: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
   });
   handleSubmit = () => {
     alert(`${this.profileForm.value.name} | ${this.profileForm.value.email}`)
